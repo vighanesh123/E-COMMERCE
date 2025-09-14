94cf4830e6a6f70732fed781f139f52001ee43a9
vighanesh123 <vighneshdabare26@gmail.com>
2025-09-14 19:47:26 +0530

    Fix Vercel build errors - disable CI warnings

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest'
+  ],
+  rules: {
+    'no-unused-vars': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'jsx-a11y/anchor-is-valid': 'warn'
+  }
+};
